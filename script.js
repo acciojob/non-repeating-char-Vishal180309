00de6fb50821819be9b0a8f2a4803aa054747792
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-06 09:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+document.getElementById('find-button').addEventListener('click', function() {
+    let inputString = document.getElementById('input-string').value;
+    let result = firstNonRepeatedChar(inputString);
+
+    if (result === null) {
+        document.getElementById('result').innerHTML = 'No non-repeating character found';
+    } else {
+        document.getElementById('result').innerHTML = 'First non-repeating character: ' + result;
+    }
+});
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+    let charCount = {};
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+
+        if (charCount[char] === undefined) {
+            charCount[char] = 1;
+        } else {
+            charCount[char]++;
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
